70cbfbb38d33d6c34ddc20886e519bb11065a699
Daw-Ran Liou <hi@dawranliou.com>
2022-10-28 16:45:05 +0800

    Update default config

--- conf.lua
+++ conf.lua
@@ -1,10 +1,10 @@
 love.conf = function(t)
    t.gammacorrect = true
-   t.title, t.identity = "change-me", "Minimal"
+   t.title, t.identity = "ctga", "ctga"
    t.modules.joystick = false
    t.modules.physics = false
-   t.window.width = 720
-   t.window.height = 450
-   t.window.vsync = false
+   -- t.window.width = 720
+   -- t.window.height = 450
+   -- t.window.vsync = false
    t.version = "11.4"
 end
